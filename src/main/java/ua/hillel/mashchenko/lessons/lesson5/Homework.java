bf13ddc5d9822feab1f592004f43a66e4d3ac0ab
Mashchenko Ilona <ilonamko.pp@gmail.com>
2023-07-17 22:21:50 +0300

    test 2 commit IDEA

--- src/main/java/ua/hillel/mashchenko/lessons/lesson5/Homework.java
+++ src/main/java/ua/hillel/mashchenko/lessons/lesson5/Homework.java
@@ -1,6 +1,8 @@
 package ua.hillel.mashchenko.lessons.lesson5;
 
 
+import java.util.Scanner;
+
 public class Homework {
 
     public static void main(String[] args) {
@@ -13,6 +15,57 @@ public class Homework {
         int cbPlayer4 = 5;
         int cbPlayer5 = 2;
 
+        Scanner scanner = new Scanner(System.in);
+        if (scanner.hasNextInt()) {
+            cbPlayer1 = scanner.nextInt();
+        } else {
+            System.out.println("Wrong data");
+            System.exit(0);
+
+        }
+
+        System.out.println("Chicago Bulls Player 1 = " + cbPlayer1);
+
+        if (scanner.hasNextInt()) {
+            cbPlayer2 = scanner.nextInt();
+        } else {
+            System.out.println("Wrong data");
+            System.exit(0);
+
+        }
+
+        System.out.println("Chicago Bulls Player 2 = " + cbPlayer2);
+
+        if (scanner.hasNextInt()) {
+            cbPlayer3 = scanner.nextInt();
+        } else {
+            System.out.println("Wrong data");
+            System.exit(0);
+
+        }
+
+        System.out.println("Chicago Bulls Player 3 = " + cbPlayer3);
+
+        if (scanner.hasNextInt()) {
+            cbPlayer4 = scanner.nextInt();
+        } else {
+            System.out.println("Wrong data");
+            System.exit(0);
+
+        }
+
+        System.out.println("Chicago Bulls Player 4 = " + cbPlayer4);
+
+        if (scanner.hasNextInt()) {
+            cbPlayer5 = scanner.nextInt();
+        } else {
+            System.out.println("Wrong data");
+            System.exit(0);
+
+        }
+
+        System.out.println("Chicago Bulls Player 5 = " + cbPlayer5);
+
         String team2 = "Los Angeles Lakers";
         int losAngelesLakersTeam = 5;
         int lalPlayer1 = 2;
@@ -20,6 +73,58 @@ public class Homework {
         int lalPlayer3 = 3;
         int lalPlayer4 = 3;
         int lalPlayer5 = 2;
+
+
+        if (scanner.hasNextInt()) {
+            lalPlayer1 = scanner.nextInt();
+        } else {
+            System.out.println("Wrong data");
+            System.exit(0);
+
+        }
+
+        System.out.println("Los Angeles Lakers Player 1 = " + lalPlayer1);
+
+        if (scanner.hasNextInt()) {
+            lalPlayer2 = scanner.nextInt();
+        } else {
+            System.out.println("Wrong data");
+            System.exit(0);
+
+        }
+
+        System.out.println("Los Angeles Lakers Player 2 = " + lalPlayer2);
+
+        if (scanner.hasNextInt()) {
+            lalPlayer3 = scanner.nextInt();
+        } else {
+            System.out.println("Wrong data");
+            System.exit(0);
+
+        }
+
+        System.out.println("Los Angeles Lakers Player 3 = " + lalPlayer3);
+
+        if (scanner.hasNextInt()) {
+            lalPlayer4 = scanner.nextInt();
+        } else {
+            System.out.println("Wrong data");
+            System.exit(0);
+
+        }
+
+        System.out.println("Los Angeles Lakers Player 4 = " + lalPlayer4);
+
+        if (scanner.hasNextInt()) {
+            lalPlayer5 = scanner.nextInt();
+        } else {
+            System.out.println("Wrong data");
+            System.exit(0);
+
+        }
+
+        System.out.println("Los Angeles Lakers Player 5 = " + lalPlayer5);
+
 
         int chicagoBullsAvg = (cbPlayer1 + cbPlayer2 + cbPlayer3 + cbPlayer4 + cbPlayer5) / chicagoBullsTeam;
         System.out.println("Chicago Bulls Avg = " + chicagoBullsAvg);
