c8b91ddefedddc7a6f1481b89e31507f82d8d928
Mashchenko Ilona <ilonamko.pp@gmail.com>
2023-07-23 08:42:19 +0300

    test 2 commit IDEA

--- src/main/java/ua/hillel/mashchenko/lessons/lesson6/Homework2.java
+++ src/main/java/ua/hillel/mashchenko/lessons/lesson6/Homework2.java
@@ -1,21 +1,37 @@
 package ua.hillel.mashchenko.lessons.lesson6;
 
-import org.w3c.dom.css.Counter;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Homework2 {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
-        for (int i = 0; i <= 100; i++) {
-            for (int j = 0; j < 100 ; j++) {
-                if (i != 4 && j != 9);
-                break;
+            int attempts = 3;
+            int secretNumber = (int) (Math.random() * 11);
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Hi! Enter a number between 0 and 10.");
+            System.out.println("You have " + attempts + " attempts.");
+
+            for (int attempt = 1; attempt <= attempts; attempt++) {
+                System.out.print("Attempt " + attempt + ": Enter your number: ");
+                int guess = scanner.nextInt();
+
+                if (guess == secretNumber) {
+                    System.out.println("You won! The correct number is " + secretNumber);
+                    break;
+                } else if (attempt < attempts) {
+                    System.out.println("Wrong number. Try again!");
+                } else {
+                    System.out.println("Sorry, you failed. The correct number was: " + secretNumber);
                 }
-            System.out.println(i);
+            }
 
+            scanner.close();
         }
 
+        private static int generateRandomNumber(int min, int max) {
+            return (int) (Math.random() * (max - min + 1)) + min;
         }
-
-
     }
 
--- src/main/java/ua/hillel/mashchenko/lessons/lesson7/Main.java
+++ src/main/java/ua/hillel/mashchenko/lessons/lesson7/Main.java
@@ -1,12 +1,14 @@
 package ua.hillel.mashchenko.lessons.lesson7;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
 
-//        int[] array = new int[5];
-
+        //int[] array = new int[5];
+//
 //       System.out.println(array.length); //довжина массиву
 //       System.out.println(array);
 //
@@ -23,11 +25,80 @@ public class Main {
 //        System.out.println(array[4]);
 
 
-//        int[] array = new int[5]{1,2,3,4,5};
+//        int[] array = {1,2,3,4,5};
 //        int[] array2 = new int[]{1,2,3,4,5};
+//
+//        System.out.println(array[4]);
+
+//        int[] array = new int [10];
+//
+//        System.out.println(0);
+//        System.out.println(2);
+
+//        int[] array = new int[25];
+//        for (int i = 0; i < array.length; i++) {
+//            array[i] = i;
+//        }
+//        for (int i = 0; i < array.length; i++) {
+//            System.out.println(array[i]);
+//        }
+////
+//
+//        System.out.println(Arrays.toString(array));
+
+//
+
+//        int[] array = new int[0];
+//        int[] array2 =
+
+//        int[] array = new int[10];
+//        for (int i = 0; i < array.length; i++) {
+//            array[i] = (int) (Math.random() * 11);
+//        }
+//
+//        for (int i = 0; i < array.length; i++) {
+//            if (i = array.length - 1) {
+//            } else {
+//                System.out.println(array[i] + ", ");
+
+
+        int[] people = new int {11, 44, 98, 44, 22, 33, 44, 91};
+        for (int i = 0; i < people.length; i++) {
+            people[i] = (int) (Math.random() * 101);
+
+        }
+
+        int max = people[0];
+
+        for (int i = 0; i < people.length; i++) {
+            if (max < people[i]) {
+                max = people[i];
+
+            }
+
+        }
+
+        System.out.println("max chance: " + max);
+
+
+
 
 
 
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
